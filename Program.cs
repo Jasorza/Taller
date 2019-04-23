114a03908c70a01b9e8a1cdcb1484505175f0585
Alejandro Sorza <Jasorza5@misena.edu.co>
2019-04-23 08:21:11 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Taller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prestamo = 17000000;
+            var interes = 0.02;
+            int plazo = 12;
+            var fecha = new DateTime();
+            var dia="";
+            var año="";
+            var mes="";
+            Console.WriteLine("Ingrese el dia: ");
+            dia = Console.ReadLine();
+            Console.WriteLine("Ingrese el mes: ");
+            mes = Console.ReadLine();
+            Console.WriteLine("ingrese el año: ");
+            año = Console.ReadLine();
+            Console.WriteLine($"La fecha ingresada es {fecha}");
+            Console.ReadLine();
+        }
+    }
+}
