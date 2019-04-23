82ff5e75bfa7d108e647febc9edb4d606a70f19d
Alejandro Sorza <Jasorza5@misena.edu.co>
2019-04-23 12:04:03 -0500

    3

--- Ejercicio/Program.cs
+++ Ejercicio/Program.cs
@@ -31,8 +31,7 @@ namespace Ejercicio
             if (dia < 5)
             { 
                 Console.WriteLine($"el valor a pagar a la fecha de hoy es de: {cuota2},{cuota},{interes2}");
-                Console.
-            
+                           
             }
             Console.ReadLine();
             
