c777b8fa0657263c4dbd2eb6d3323745ae144d48
Alejandro Sorza <Jasorza5@misena.edu.co>
2019-04-23 13:36:36 -0500

    4

--- Ejercicio/Program.cs
+++ Ejercicio/Program.cs
@@ -6,32 +6,32 @@ namespace Ejercicio
     {
         static void Main(string[] args)
         {
-            int interes = 2 / 100;
-            int plazo = 12;
-            int dia = 1;
-            int cuota = (interes * 17000000);
-            double interes2 = (1 - (1 + interes));
-                double interestotal= Math.Pow(interes2,-12);
-            double cuota2 = (interestotal / cuota);
-            //DateTime consignacion;
-            //Console.WriteLine("::::::::::: Mariana, Bienevenida al Banco BBVA :::::::::::");
-            //Console.WriteLine("Por favor ingresa la fecha:");
-            //Console.WriteLine("Ingrese el dia: ");
-            //Console.ReadLine();
-            //Console.WriteLine("Ingrese el mes: ");
-            //Console.ReadLine();
-            //Console.WriteLine("ingrese el año: ");
-            //Console.ReadLine();
-            //consignacion = Convert.ToDateTime(Console.ReadLine());
-            //int dia = consignacion.Day;
-            //int mes = consignacion.Month;
-            //int año = consignacion.Year;
-            //Console.WriteLine($"La fecha ingresada es {dia}/{mes}/{año}");
-            //
+            double interes = 0.02;
+            var dia = 10;
+            double cuota = 340000;
+            double interes2 = 0.21;
+            double interestotal = 1619047;
+            double diasmora = dia-5;
+            double interesmora = interestotal*0.02*diasmora;
+            
+            Console.WriteLine("::::::::::: Mariana, Bienevenida al Banco BBVA :::::::::::");
+            Console.WriteLine("Por favor ingresa la fecha:");
+            Console.WriteLine("Ingrese el dia: ");
+            dia = Console.ReadLine();
+            Console.WriteLine("Ingrese el mes: ");
+            Console.ReadLine();
+            Console.WriteLine("ingrese el año: ");
+            Console.ReadLine();
+            Console.WriteLine($"La fecha ingresada es {dia}/{mes}/{año}");
+            
             if (dia < 5)
             { 
-                Console.WriteLine($"el valor a pagar a la fecha de hoy es de: {cuota2},{cuota},{interes2}");
-                           
+                Console.WriteLine($"el valor a pagar a la fecha de hoy es de: {interestotal}");                        
+            }
+            else
+            {
+                Console.WriteLine($"la cantidad de dias en mora es de: {diasmora}");
+                Console.WriteLine($"el total a pagar por los dias en mora es de: {interestotal + interesmora}");
             }
             Console.ReadLine();
             
